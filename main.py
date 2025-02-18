831300a5cff5a6ed9b6bbdb1800ef673ce2d05a9
Mateusz Lewandowski <lewandowski.mattt@gmail.com>
2025-02-18 16:55:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from game import CharacterMenu
-from game import World
-from game import ValidationError
+from game import CharacterMenu, World, ValidationError
+from sql_connection import CharacterManager, Database
 
 class Game:
     def __init__(self):
@@ -11,7 +10,7 @@ class Game:
         input_outfit = int(input("Choose your outfit (1 or 2):\n"))
         input_character_kind = input("Choose your character's kind ('Warrior', 'Sura', 'Ninja', 'Shaman'):\n")
         input_kingdom = input("Choose your kingdom ('Chunjo', 'Jinno', 'Shinsoo'):\n")
-        input_mode = input("Choose your mode ('singleplayer', 'multiplayer'):\n")
+        input_mode = input("Choose your mode (singleplayer, multiplayer):\n")
 
         try:
             self.chosen_character = CharacterMenu(input_name, mode=input_mode, outfit=input_outfit, character_kind=input_character_kind, kingdom=input_kingdom)
@@ -23,9 +22,18 @@ class Game:
         CharacterMenu.screen_loading()
 
         world = World(self.chosen_character)
-
         world.welcome_message()
-        world.map_m1()
+
+        db = Database()
+        character_manager = CharacterManager(db)
+
+        character_manager.add_character(self.chosen_character)
+
+        character_manager.display_characters()
+
+        character_manager.close()
+
+        #world.map_m1()
 
 if __name__ == '__main__':
     game = Game()
